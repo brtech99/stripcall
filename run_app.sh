ea9f941000f176cea5379f87ed2fc4fa2c3834d3
Brian Rosen <brian.rosen@gmail.com>
2025-08-01 17:07:37 -0400

    Remove sensitive files from tracking

--- run_app.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-
-# Run the Flutter app with the defined variables
-flutter run -v --dart-define=SUPABASE_URL=https://wpytorahphbnzgikowgz.supabase.co --dart-define=SUPABASE_ANON_KEY=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndweXRvcmFocGhibnpnaWtvd2d6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzYxMjA4ODYsImV4cCI6MjA1MTY5Njg4Nn0.3DFQacxVHHoIeS6wb1GU1GSNQ__Mbmt6SDiZFjwHuWg
